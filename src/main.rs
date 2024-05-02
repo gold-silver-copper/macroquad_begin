734076c71ca5a128fe69011604b26732d080cf61
zombkit <stephen.korzen@gmail.com>
2024-05-02 09:05:53 -0700

    a

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,31 @@ use ratatui::{
 };
 use ratframe::RataguiBackend;
 
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Branch {
+    pub name: String,
+    pub commit: Commit,
+}
+
 #[macroquad::main("egui with macroquad")]
 async fn main() {
     let boop = RataguiBackend::new(100, 50);
     let mut terminal = Terminal::new(boop).unwrap();
+
+    let res = reqwest::Client::new()
+    .get("https://api.github.com/repos/rustwasm/wasm-bindgen/branches/master")
+    .header("Accept", "application/vnd.github.v3+json")
+    .send()
+    .await?;
+
+let text = res.text().await?;
+let branch_info: Branch = serde_json::from_str(&text).unwrap();
+
+    
     loop {
         clear_background(WHITE);
 
